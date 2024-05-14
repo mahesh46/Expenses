bd1d274292720645ae1e401c2222f8f99dc98cbc
mahesh46 <mlad.mahesh.lad@gmail.com>
2024-05-14 16:05:10 +0100

    updated preview so runs on older iphones

--- Expenses/ContentView.swift
+++ Expenses/ContentView.swift
@@ -61,12 +61,12 @@ struct ContentView: View {
     
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-//    }
-//}
-#Preview {
-    ContentView()
-        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
+//#Preview {
+//    ContentView()
+//        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+//}
